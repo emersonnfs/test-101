9812bd06585dbe3aafa2d4d81f702a5e15d72d8b
emersonnfs <rm95199@fiap.com.br>
2023-01-26 17:28:12 -0300

    ajust moving

--- src/components/Foto.jsx
+++ src/components/Foto.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import Vestido from '../img/vestido.png';
 //import Estampa from '../img/estampaTest.jpg';
-import { Img } from "../style/styled";
+import { Img, DivGeral, DivSlider } from "../style/styled";
+//import RangeSlider from 'react-range-slider-input';
+
 
 
 export default function Foto() {
-    const el = document.getElementById('module');
 
-    el.addEventListener('mousemove', (e) => {
-    el.style.setProperty('---x', -e.offsetX + "px");
-    el.style.setProperty('---y', -e.offsetY + "px");
-});
+    document.addEventListener('dragover', (event) =>{
+          
+        const objeto = document.getElementById('module');
+        // Grab the mouse's X-position.
+        const mousex = -event.offsetX;
+        // Grab the mouse's Y-position.
+        const mousey = -event.offsetY;
+
+        objeto.style.backgroundPosition = mousex + 'px ' + mousey + 'px';
+    });
+
     return (
-        <div>
-            <Img src={Vestido} alt='camiseta' class='module' id='module'/>
-        </div>
+        <DivGeral>
+            <div>
+                <Img src={Vestido} alt='camiseta' class='module' id='module'/>
+            </div>
+            <DivSlider class='sliderContainer'>
+                <input type='range' min='7' max ='100' valeu='47' step='1' id='myRange' class='slider' />
+            </DivSlider>
+        </DivGeral>
     )
 }
--- src/style/styled.js
+++ src/style/styled.js
@@ -3,14 +3,59 @@ import Estampa from '../img/estampaTest2.jpg'
 import Vestido from '../img/vestido.png'
 
 export const Img = styled.img`
+    --size : 46%;
     width: 50%;
     height: 50%;
     background-image: url(${Estampa});
-    ---x: 0px;
-    ---y: 0px;
-    background-position: var(---x) var(---y);
-    background-size: 7%;
+    background-size: var(--size);
     background-repeat: repeat;
     -webkit-mask-image: url(${Vestido}) center/cover ;
     mask-image: url(${Vestido}) center/cover;
 `
+
+export const DivGeral = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: fit-content;
+    width: fit-content;
+    background-color: #f5f5f5;
+`
+
+export const DivSlider = styled.div`
+    .slider {
+        -webkit-appearance: none;  /* Override default CSS styles */
+        appearance: none;
+        width: 200px;
+        height: 25px; /* Specified height */
+        background: #d3d3d3; /* Grey background */
+        outline: none; /* Remove outline */
+        opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+        -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+        transition: opacity .2s;
+    }
+
+    /* Mouse-over effects */
+    .slider:hover {
+        opacity: 1; /* Fully shown on mouse-over */
+    }
+
+    /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+    /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none; /* Override default look */
+        appearance: none;
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #04AA6D; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+    }
+
+    .slider::-moz-range-thumb {
+        width: 25px; /* Set a specific slider handle width */
+        height: 25px; /* Slider handle height */
+        background: #04AA6D; /* Green background */
+        cursor: pointer; /* Cursor on hover */
+    }
+`
+
